fe5d5cf3749cb12e30b1920758157ee83db0c2aa
quinnwai <quinnwai.wong@gmail.com>
2025-01-24 13:50:34 -0800

    update readme

--- src/plugin_system/plugins/simple_plugin.py
+++ src/plugin_system/plugins/simple_plugin.py
@@ -3,5 +3,5 @@ from plugin_system.plugins.base_plugin import BasePlugin
 
 class SimplePlugin(BasePlugin):
     """
-    Simple example plugin using all default methods from the BasePlugin.
+    Simple example plugin that automatically inherits all default methods from the BasePlugin.
     """
